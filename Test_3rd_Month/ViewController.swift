0bc2407df5466eb3b0e15d014b263908fa14eb2e
Bak8 <ataibegaliev@gmail.com>
2022-12-12 20:15:27 +0600

    Initial Commit

---
+++ Test_3rd_Month/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_3rd_Month
+//
+//  Created by Atai Begaliev on 12/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
